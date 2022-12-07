58e654936f0c97166fab513e12918abb5fee4276
Chris Dutton <chris@aquatic-edge.com>
2022-12-07 13:22:08 -0800

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GlobalProcess.MixProject do
   def project do
     [
       app: :global_process,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
